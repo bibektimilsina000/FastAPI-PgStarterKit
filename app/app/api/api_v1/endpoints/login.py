8b8c578b3915157a4c8b6d143aa60accafb4b3cd
Bibek Timilsina <bibektimilsina000@gmail.com>
2023-08-04 15:37:16 +0545

    deploy

--- app/app/api/api_v1/endpoints/login.py
+++ app/app/api/api_v1/endpoints/login.py
@@ -36,13 +36,13 @@ def login_access_token(
     elif not crud.user.is_active(user):
         raise HTTPException(status_code=400, detail="Inactive user")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
     return {
         "access_token": security.create_access_token(
             user.id, expires_delta=access_token_expires
         ),
         "token_type": "bearer",
         "role": user.role,
-        "id": user.id,
     }
 
 
