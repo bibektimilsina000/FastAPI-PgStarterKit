2fd6f326ece87be2236f596c8b5589715194904f
Gijs Mos <gijs.mos@matanga.nl>
2023-09-18 16:50:53 +0200

    Improved handling of environment variables and secrets handling and docs.
    - Made a template for .env
    - Removed .env from git (beware: the one from before is still in. Never reuse stuff from there)
    - Update documentation with info how to set-up .env and run the app in dev/test mode.
    - Removed obsolete config values from core/config.py

--- app/core/config.py
+++ app/core/config.py
@@ -12,7 +12,6 @@ class Settings(BaseSettings):
     SECRET_KEY: str = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
-    SERVER_NAME: str
     SERVER_HOST: AnyHttpUrl
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
@@ -29,14 +28,6 @@ class Settings(BaseSettings):
         raise ValueError(v)
 
     PROJECT_NAME: str
-    SENTRY_DSN: Optional[HttpUrl] = None
-
-    @field_validator("SENTRY_DSN", mode="before")
-    @classmethod
-    def sentry_dsn_can_be_blank(cls, v: Optional[str]) -> Optional[str]:
-        if v is None or len(v) == 0:
-            return None
-        return v
 
     POSTGRES_SERVER: str
     POSTGRES_USER: str
@@ -91,6 +82,7 @@ class Settings(BaseSettings):
     FIRST_SUPERUSER: EmailStr
     FIRST_SUPERUSER_PASSWORD: str
     USERS_OPEN_REGISTRATION: bool = False
+    
     model_config = SettingsConfigDict(case_sensitive=True)
 
 
