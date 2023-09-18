290d2c9ec4af30348350eadbf606a2acebac1938
Gijs Mos <gijs.mos@matanga.nl>
2023-09-18 10:26:46 +0200

    Removed Celery support

--- app/api/api_v1/endpoints/utils.py
+++ app/api/api_v1/endpoints/utils.py
@@ -5,22 +5,10 @@ from pydantic.networks import EmailStr
 
 from app import models, schemas
 from app.api import deps
-from app.core.celery_app import celery_app
+
 from app.utils import send_test_email
 
 router = APIRouter()
-
-
-@router.post("/test-celery/", response_model=schemas.Msg, status_code=201)
-def test_celery(
-    msg: schemas.Msg,
-    current_user: models.User = Depends(deps.get_current_active_superuser),
-) -> Any:
-    """
-    Test Celery worker.
-    """
-    celery_app.send_task("app.worker.test_celery", args=[msg.msg])
-    return {"msg": "Word received"}
 
 
 @router.post("/test-email/", response_model=schemas.Msg, status_code=201)
